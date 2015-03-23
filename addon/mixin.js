332a222f56b6487fd9703d229346b9ba8163bd9f
namlook <nicolas.clairon@elkorado.com>
2015-03-23 12:19:44 +0100

    upload all files when the user click on save

--- addon/mixin.js
+++ addon/mixin.js
@@ -5,6 +5,7 @@ export default Ember.Mixin.create({
     acceptedMimeTypes: null,
     maxFilesize: 250, // in MB
     maxFiles: 20,
+    autoProcessQueue: false,
     _dropzone: null,
 
     url: function() {
@@ -16,36 +17,75 @@ export default Ember.Mixin.create({
      */
     onfileUploaded: Ember.required,
 
-    insertDropzone: function() {
+
+    /** returns the dropzone's accept function **/
+    dropzoneAccept: function() {
+        var model = this.get('model');
+        return function(file, done) {
+            // 'this' represent the dropzone context here
+            done();
+            var dzContext = this;
+            var scheduledFn = function() {
+                return new Ember.RSVP.Promise(function(resolve, reject) {
+                    dzContext.processQueue();
+                    dzContext.on('queuecomplete', function() {
+                        Ember.run.next(function() {
+                            return resolve();
+                        });
+                    });
+                    dzContext.on('error', function(error, bla) {
+                        console.log('error>', error, bla);
+                        Ember.run.next(function() {
+                            return reject(error);
+                        });
+                    });
+                });
+            };
+            model._scheduleFor('save', 'uploadFiles', scheduledFn);
+        };
+    },
+
+    dropzoneSuccess: function() {
         var that = this;
+        return function(file, res){
+            if (file.previewElement) {
+                file.previewElement.classList.add("dz-success");
+            }
+
+            var record = {
+                title: file.name,
+                size: file.size,
+                path: res.path,
+                thumbPath: res.thumbPath,
+                type: file.type,
+                lastModified: file.lastModifiedDate
+            };
+
+            var dzContext = this;
+            var fileRecord = that.get('store.db')['File'].createRecord(record);
+            that.onfileUploaded(fileRecord);
+
+            dzContext.removeFile(file);
+            Ember.run.next(function() {
+                dzContext.processQueue();
+            });
+        };
+    },
+
+    _insertDropzone: function() {
         var dropozone = new Dropzone(this.$('.dropzone')[0], {
             url: this.get('url'),
-            autoProcessQueue: true,
+            autoProcessQueue: this.get('autoProcessQueue'),
             maxFiles: this.get('maxFiles'),
             addRemoveLinks: true,
             // dictRemoveFile: 'reeeeemove',
             previewTemplate: this.$('.dz-template').html(),
             maxFilesize: this.get('maxFilesize'),
         //       acceptedFiles: this.get('acceptedMimeTypes'),
-            success: function(file, res){
-                if (file.previewElement) {
-                    file.previewElement.classList.add("dz-success");
-                }
-
-                var record = {
-                    title: file.name,
-                    size: file.size,
-                    path: res.path,
-                    thumbPath: res.thumbPath,
-                    type: file.type,
-                    lastModified: file.lastModifiedDate
-                };
-
-                var fileRecord = that.get('store.db')['File'].createRecord(record);
-                that.onfileUploaded(fileRecord);
-                this.removeFile(file);
-            },
+            success: this.dropzoneSuccess(),
+            accept: this.dropzoneAccept(),
             maxfilesexceeded: function(file, bla, foo) {
+                // this.removeFile(file);
                 console.log('maxfilesexceeded>>>', file, bla, foo);
             }
         });
